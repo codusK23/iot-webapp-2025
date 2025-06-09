4caace5b59ec763b495dde8b8f64c785ab785ca6
codusK23 <codusK23@gmail.com>
2025-06-09 18:01:38 +0900

    day11 commit

---
+++ MyPortfolioWeb/MyPortfolioWeb/MyPortfolioWebApp/Models/Board.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyPortfolioWebApp.Models;
+
+public class Board
+{
+    [Key]
+    [DisplayName("번호")]
+    public int Id { get; set; }
+
+    [DisplayName("이메일")]
+    [BindNever]
+    public string Email { get; set; }
+
+    [DisplayName("작성자")]
+    [BindNever]
+    public string? Writer { get; set; }
+
+    [DisplayName("제목")]
+    [Required(ErrorMessage = "{0}은 필수입니다.")]
+    public string Title { get; set; }
+
+    [DisplayName("내용")]
+    [Required(ErrorMessage = "{0}은 필수입니다.")]
+    public string Contents { get; set; }
+
+    [DisplayName("작성일")]
+    [DisplayFormat(DataFormatString = "{0:yyyy년 MM월 dd일}", ApplyFormatInEditMode = true)]
+    [BindNever]
+    public DateTime? PostDate { get; set; }
+
+    [DisplayName("조회수")]
+    [BindNever]
+    public int? ReadCount { get; set; }
+}
+
---
+++ day11/Day11Study/WebApiApp02/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiApp02.Models
+{
+    public class Book
+    {
+        [Key]
+        // Key
+        public int Idx { get; set; }
+
+        [Required]
+        // 책제목
+        public string Names { get; set; }
+
+        [Required]
+        // 책저자
+        public string Author { get; set; }
+        
+        [Required]
+        // 출판일
+        public DateOnly ReleaseDate { get; set; }
+    }
+}
